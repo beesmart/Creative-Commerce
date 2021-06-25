804929cdeaa2ba467d7e633ab4c8e5075250dc6e
Stephen Collins <stephen.collins@digitalzest.co.uk>
2021-06-25 12:02:49 +0100

    Removed unwanted directories

--- codepress-admin-columns/templates/admin/page/settings-section.php
+++
@@ -1,9 +0,0 @@
-<tr class="<?= esc_attr( $this->class ); ?>">
-	<th scope="row">
-		<h2><?= $this->title; ?></h2>
-		<p><?= $this->description; ?></p>
-	</th>
-	<td>
-		<?= $this->content; ?>
-	</td>
-</tr>
